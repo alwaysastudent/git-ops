5bab5e98cea7d6f0c7cd8e259d9c366bda4b4519
fribeiro <fabiomatcomp@gmail.com>
2020-12-09 12:54:50 +0000

    gitops operator development

---
+++ imperative-deployment.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+kubectl create deployment nginx-imperative --image=nginx:latest
+kubectl scale deployment/nginx-imperative --replicas 3
+kubectl annotate deployment/nginx-imperative environment=pros
+kubectl annotate deployment/nginx-imperative organization=sales
